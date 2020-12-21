58d51364167aa254e1b4ba66e8138bfc1a4c735b
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-12-21 15:08:05 +0000

    added json server and fetch

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem
 import Link from 'react-router-dom/Link';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -108,7 +109,7 @@ function RenderDish({dish}){
 
         return(
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
